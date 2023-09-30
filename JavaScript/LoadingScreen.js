bb922ca744a54e1e507f280935e72fc32c387896
OverlordSock <133079441+OverlordSock@users.noreply.github.com>
2023-09-30 17:50:09 +1300

    Update LoadingScreen.js

--- JavaScript/LoadingScreen.js
+++ JavaScript/LoadingScreen.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function() {
     const loadingScreen = document.getElementById('loadingScreen');
     
-    // Hide the loading screen after 2 seconds (2000 milliseconds)
+    // Hide the loading screen after 0.5 seconds (500 milliseconds)
     setTimeout(function() {
         loadingScreen.style.display = 'none';
-    }, 1000);
+    }, 500);
 });
